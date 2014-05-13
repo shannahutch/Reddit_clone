974eaca37ae9485064620cecfb723aa2e81b5722
Githubshanna <shannahutch@gmail.com>
2014-05-13 12:28:14 -0700

    added devise and bootstrap file

---
+++ reddit_clone/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = '1411931865d542ba0032c8366b034c07ef810f3e82de55ecd7ab3bec2c1711e5f0c9c4b99d483c3f7c774f1d218aee33ac310a54889de1e9a31142d9d4ffbf51'
