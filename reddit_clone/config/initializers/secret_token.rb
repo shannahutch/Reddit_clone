80a4da56feed51b99f0204432e66dd619ab4ee5b
Githubshanna <shannahutch@gmail.com>
2014-05-13 17:15:53 -0700

    made models for post and comment

--- reddit_clone/config/initializers/secret_token.rb
+++ reddit_clone/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-RedditClone::Application.config.secret_key_base = '1411931865d542ba0032c8366b034c07ef810f3e82de55ecd7ab3bec2c1711e5f0c9c4b99d483c3f7c774f1d218aee33ac310a54889de1e9a31142d9d4ffbf51'
+RedditClone::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
